6317c27078eaa0223066e1366e9c7f2a4bd62e9e
Carlos Calgaro <carloscalgarof@gmail.com>
2026-01-21 16:34:28 -0300

    fix: Grabbing idempotency token from header

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -1,15 +1,16 @@
 class Api::V1::PaymentsController < ApplicationController
 
+  before_action :set_idempotency_key, only: [:post, :confirm, :refund]
   before_action :set_payment_intent, only: [:confirm, :refund]
   def post
-    payment_intent = PaymentIntent.find_by(idempotency_key: payment_intent_params[:idempotency_key])
+    payment_intent = PaymentIntent.find_by(idempotency_key: @idempotency_key)
 
     return render json: payment_intent, status: :ok if payment_intent.present?
 
     payment_intent = PaymentIntents::CreatePaymentIntentService.call(
       amount_cents: payment_intent_params[:amount_cents],
       currency: payment_intent_params[:currency],
-      idempotency_key: payment_intent_params[:idempotency_key]
+      idempotency_key: @idempotency_key
     )
 
     render json: payment_intent, status: :created
@@ -17,9 +18,15 @@ class Api::V1::PaymentsController < ApplicationController
 
 
   def confirm
-    
-  end
+    if @payment_intent && @idempotency_key && @payment_intent.confirmation_idempotency_key.nil?
+      PaymentIntents::ConfirmPaymentIntentService.call(
+        payment_intent_id: @payment_intent.id,
+        idempotency_key: @idempotency_key
+      )
+    end
 
+    render status: :accepted
+  end
 
   def refund
     render "Refunding"
@@ -32,15 +39,14 @@ class Api::V1::PaymentsController < ApplicationController
     @payment_intent = PaymentIntent.find(params[:id])
   end
 
-  def confirm_payment_intent_params
-    params.require(:payment_intent).permit(:id)
+  def set_idempotency_key
+    @idempotency_key = request.headers["Idempotency-Key"]
   end
 
   def payment_intent_params
     params.require(:payment_intent).permit(
       :amount_cents,
       :currency,
-      :idempotency_key,
     )
   end
 end
